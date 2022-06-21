9920daa8c2a45523cb18fb9e7aa1af749380fd3d
Hrish <coderhb00@gmail.com>
2022-06-21 10:55:48 +0530

    setup completed but very basic

---
+++ src/components/Success.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Dialog from '@mui/material/Dialog';
+import AppBar from '@mui/material/AppBar';
+import { ThemeProvider as MuiThemeProvider } from '@mui/material/styles';
+
+export class Success extends Component {
+  render() {
+    return (
+      // <MuiThemeProvider>
+        <>
+          <Dialog
+            open
+            fullWidth
+            maxWidth='sm'
+          >
+            <AppBar title="Success" />
+            <h1>Thank You For Your Submission</h1>
+            <p>You will get an email with further instructions.</p>
+          </Dialog>
+        </>
+      // </MuiThemeProvider>
+    );
+  }
+}
+
+export default Success;
